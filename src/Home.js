45ab426bd7a2aa90d786ad3b59edb6558ba68978
Fernando Ott <ferando.ott8@gmail.com>
2024-05-20 13:10:16 +0200

    text effekt/cursor finish

--- src/Home.js
+++ src/Home.js
@@ -61,3 +61,34 @@ links.forEach((link) => {
     dot.style.backgroundColor = "#3700ff";
   });
 });
+
+// Schrift effekt
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h1").onmouseover = (event) => {
+  let iteration = 0;
+
+  clearInterval(interval);
+
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+
+    iteration += 1 / 3;
+  }, 25);
+};
